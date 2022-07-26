0f2579bfc733da2dd10925e89f531578b148ef08
zarni-ein <filippov@mail.ch>
2022-07-26 22:01:21 +0300

    OOPified GUI version, added sequence and divider customization

--- sopg_gui.py
+++ sopg_gui.py
@@ -2,76 +2,86 @@ import tkinter as tk
 import sopg_cli as core
 
 
-def to_clipboard(root: tk.Tk, password: core.Password):
-    root.clipboard_clear()
-    root.clipboard_append(password.get())
+class App:
+    def to_clipboard(self, root: tk.Tk):
+        root.clipboard_clear()
+        root.clipboard_append(self.password.get())
 
+    def update_param(self):
+        self.password.leetrules = self.leetrules if self.leetify_var.get() else {}
+        self.password.set_sequence(self.sequence_field.get())
+        self.password.set_divider(self.divider_field.get())
 
-def regen_one(password: core.Password, buttons: list[tk.Button], button_frame: tk.Frame, number: int):
-    password.leetrules = leetrules if leetify_var.get() else {}
-    password.regen_one(number)
-    update_buttons(password, buttons, button_frame)
+    def regen_one(self, number: int):
+        self.update_param()
+        self.password.regen_one(number)
+        self.update_buttons()
 
+    def regen_whole(self):
+        self.update_param()
+        self.password.regen_whole()
+        self.update_buttons()
 
-def regen_whole(password: core.Password, buttons: list[tk.Button], button_frame: tk.Frame):
-    password.leetrules = leetrules if leetify_var.get() else {}
-    password.regen_whole()
-    update_buttons(password, buttons, button_frame)
+    def update_buttons(self):
+        for button in self.buttons:
+            button.destroy()
+        self.buttons = []
+        for i in range(len(self.password.words)):
+            self.buttons.append(tk.Button(self.button_frame, text=self.password.words[i], command=lambda x = i: self.regen_one(x)))
+            self.buttons[i].pack(side=tk.LEFT)
+        self.buttons.append(tk.Button(self.management_frame, text="Regen PW", command=lambda: self.regen_whole()))
+        self.buttons[-1].grid(row=0,column=0)
 
+    def __init__(self, root):
+        self.password = core.Password()
+        self.leetrules = {
+                'O': '0',
+                'o': '0',
+                'I': '1',
+                'i': '1',
+                'B': '8',
+                'b': '8',
+                'S': '$',
+                's': '$',
+                'L': '!',
+                'l': '!'
+        }
 
-def update_buttons(password: core.Password, buttons: list[tk.Button], button_frame: tk.Frame):
-    for button in buttons:
-        button.destroy()
-    buttons = []
-    for i in range(len(password.words)):
-        buttons.append(tk.Button(button_frame, text=password.words[i], command=lambda x = i: regen_one(password, buttons, button_frame, x)))
-        buttons[i].pack(side=tk.LEFT)
-    buttons.append(tk.Button(button_frame, text="Regen PW", command=lambda: regen_whole(password, buttons, button_frame)))
-    buttons[-1].pack(side=tk.LEFT)
+        self.header = tk.LabelFrame(root, text="Header")
+        self.header.pack(fill=tk.X)
 
+        self.button_frame = tk.LabelFrame(self.header, text="Button frame")
+        self.button_frame.pack(fill=tk.X, side=tk.LEFT)
+        self.management_frame = tk.LabelFrame(self.header, text="MGMT frame")
+        self.management_frame.pack(side=tk.RIGHT)
+        self.structure_frame = tk.Frame(root)
+        self.structure_frame.pack()
 
-def main():
-    global buttons
-    global leetrules
-    global leetify_var
-    password = core.Password()
-    leetrules = {
-            'O': '0',
-            'o': '0',
-            'I': '1',
-            'i': '1',
-            'B': '8',
-            'b': '8',
-            'S': '$',
-            's': '$',
-            'L': '!',
-            'l': '!'
-    }
-    root = tk.Tk()
-    root.title("Secure-Obscure Password Generator")
+        self.copy_button = tk.Button(self.management_frame, text="Copy to CB", command=lambda: self.to_clipboard(root))
+        self.copy_button.grid(row=0,column=1)
 
-    header = tk.Frame(root)
-    header.pack()
+        self.leetify_var = tk.BooleanVar()
+        self.leetify_box = tk.Checkbutton(self.management_frame, text='Leetify', variable=self.leetify_var, offvalue=False, onvalue=True)
+        self.leetify_box.grid(row=0,column=2)
+        self.leetify_box.select()
 
-    button_frame = tk.Frame(header)
-    button_frame.grid(row=0,column=0)
-    mgmt_frame = tk.Frame(header)
-    mgmt_frame.grid(row=0,column=1)
+        self.sequence_frame = tk.LabelFrame(self.structure_frame, text='Sequence')
+        self.sequence_frame.pack(side=tk.LEFT)
+        self.sequence_field = tk.Entry(self.sequence_frame, width=100)
+        self.sequence_field.insert(0, 'adj,nou,ver,adv')
+        self.sequence_field.pack()
+        self.divider_frame = tk.LabelFrame(self.structure_frame, text='Divider')
+        self.divider_frame.pack(side=tk.LEFT)
+        self.divider_field = tk.Entry(self.divider_frame, width=15)
+        self.divider_field.insert(0, '/')
+        self.divider_field.pack()
 
-    buttons = []
-    update_buttons(password, buttons, button_frame)
-
-    copy_button = tk.Button(mgmt_frame, text="Copy to CB", command=lambda: to_clipboard(root, password))
-    copy_button.pack(side=tk.LEFT)
-
-    leetify_var = tk.BooleanVar()
-    leetify_box = tk.Checkbutton(mgmt_frame, text='Leetify', variable=leetify_var, offvalue=False, onvalue=True)
-    leetify_box.pack(side=tk.LEFT)
-    leetify_box.select()
-
-
-    root.mainloop()
+        self.buttons = []
+        self.update_buttons()
 
 
 if __name__ == '__main__':
-    main()
+    root = tk.Tk()
+    root.title("Secure-Obscure Password Generator")
+    app = App(root)
+    root.mainloop()
